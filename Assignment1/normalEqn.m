9e4eab5d06068e612f9675873371abfcc8581b3a
Taha Aksu <tahaaksu01@gmail.com>
2017-07-04 11:37:06 +0300

    Linear Regression with Multiple Variables

---
+++ Assignment1/normalEqn.m
@@ -0,0 +1,23 @@
+function [theta] = normalEqn(X, y)
+%NORMALEQN Computes the closed-form solution to linear regression 
+%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%   regression using the normal equations.
+
+theta = zeros(size(X, 2), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the code to compute the closed form solution
+%               to linear regression and put the result in theta.
+%
+
+% ---------------------- Sample Solution ----------------------
+tX=X';
+theta=pinv(tX*X)*tX*y
+
+
+% -------------------------------------------------------------
+
+
+% ============================================================
+
+end
